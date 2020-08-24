a971dadfb9ec8ea24d1eee8724eef85328c85919
William Donovan-Seid <wdstwo@live.com>
2020-08-24 08:57:34 -0700

    add scripts for findContact and deleteContact
    co-author: Ophelia Swen <ophelia.swen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,29 @@ AddressBook.prototype.assignId = function() {
   return this.currentId;
 }
 
+AddressBook.prototype.findContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts [i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
 function Contact(firstName, lastName, phonenumber) {
   this.firstName = firstName;
   this.lastName = lastName;
